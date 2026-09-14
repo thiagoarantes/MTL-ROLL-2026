9ffaa3d3dc41222708d3c1c22729928af8b89bbc
Thiago Arantes <thiagoarantes@gmail.com>
2026-09-14 12:18:43 -0400

    feat: update sponsor registry
    Add Endless Blading Co. and remove Xact Skate Shop.

--- src/data/index.ts
+++ src/data/index.ts
@@ -226,12 +226,6 @@ export const SPONSORS: Sponsor[] = [
     image: '/sponsors/solo.png',
     website: 'https://solo-inline.com/'
   },
-  // {
-  //   id: 'spon-xact',
-  //   name: 'Xact Skate Shop',
-  //   image: '/sponsors/xact.png',
-  //   website: 'https://xactskateshop.com/'
-  // },
   {
     id: 'spon-lowlife',
     name: 'Lowlife Mtl',
@@ -261,5 +255,11 @@ export const SPONSORS: Sponsor[] = [
     name: 'YOYOSKATE',
     image: '/sponsors/yoyo.png',
     website: 'https://www.yoyoskateofficial.com/'
+  },
+  {
+    id: 'spon-endless',
+    name: 'Endless Blading Co.',
+    image: '/sponsors/endless.png',
+    website: 'https://www.endlessblading.com/'
   }
 ];
